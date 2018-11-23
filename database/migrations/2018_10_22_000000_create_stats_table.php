0ac342015af59082b6383f648d8a7d086d310a51
Damien Rabellino <damien@cyberfab.fr>
2018-11-23 17:20:33 +0100

    Migration pour stats

--- database/migrations/2018_10_22_000000_create_stats_table.php
+++ database/migrations/2018_10_22_000000_create_stats_table.php
@@ -17,18 +17,8 @@ class CreateStatsTable extends Migration
             $table->increments('id_stats');
             $table->string('id_util', 45)->unsigned();
             $table->foreign('id_util')->references('id')->on('utilisateurs')->onDelete('cascade');
-            $table->integer('adresse1', 255)->nullable();
-            $table->string('adresse2', 255)->nullable();
-            $table->string('adresse3', 255)->nullable();
-            $table->string('code_postal', 10)->nullable();
-            $table->string('ville', 45)->nullable();
-            $table->decimal('latitude', 12, 6)->nullable();
-            $table->decimal('longitute', 12, 6)->nullable();
-            $table->string('nom_occupant', 45)->nullable();
-            $table->string('prenom_occupant', 45)->nullable();
-            $table->string('commentaire', 500)->nullable();
-            $table->integer('esp_utilisateur_idutilisateur')->unsigned();
-            $table->foreign('esp_utilisateur_idutilisateur')->references('idutilisateur')->on('esp_utilisateur')->onDelete('cascade');
+            $table->integer('xp')->nullable();
+            $table->string('string', 255);
         });
     }
 
