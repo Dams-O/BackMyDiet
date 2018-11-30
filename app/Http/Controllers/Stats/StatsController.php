86a216c207eab32d30e0b1c7ff211525136c5427
Unknown <jxerri145@gmail.com>
2018-11-30 17:52:09 +0100

    Donnée Suivi View, controller, front

--- app/Http/Controllers/Stats/StatsController.php
+++ app/Http/Controllers/Stats/StatsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Stats;
 use App\Http\Controllers\Controller;
 use App\Models\Stats;
 use App\User;
+use App\Models\DonneeSuivi;
 
 class StatsController extends Controller
 {
@@ -41,10 +42,11 @@ class StatsController extends Controller
     {
         $users = User::where('id_utilisateur', $id)->first();
         $stats = Stats::where('id_util', $id)->get();
-        // var_dump($stats);
+        $ds = DonneeSuivi::where('id_util', $id)->get();
+        // var_dump($stats);    
         // exit;
 
         
-        return view('front.profilStats', array('users' => $users, 'stats' => $stats));
+        return view('front.profilStats', array('users' => $users, 'stats' => $stats, 'ds' => $ds));
     }
 }
--- resources/views/front/profilStats.blade.php
+++ resources/views/front/profilStats.blade.php
@@ -20,4 +20,33 @@
         </tbody>
     </table>
 
+    <table class="table table-hover">
+        <thead>
+            <tr class="success">
+                <th>Calcium</th>
+                <th>Protéine</th>
+                <th>GL</th>
+                <th>FVSM</th>
+                <th>MG</th>
+                <th>Sucre</th>
+                <th>Score</th>
+                <th>Crée le</th>
+            </tr>
+        </thead>
+        <tbody>
+        @foreach ($ds as $donnee)
+            <tr>
+                <td><strong><?php echo $donnee->calcium; ?></strong></td>
+                <td><?php echo $donnee->prot; ?></td>
+                <td><strong><?php echo $donnee->GL; ?></strong></td>
+                <td><?php echo $donnee->FVSM; ?></td>
+                <td><strong><?php echo $donnee->MG; ?></strong></td>
+                <td><?php echo $donnee->sucre; ?></td>
+                <td><strong><?php echo $donnee->score; ?></strong></td>
+                <td><?php echo $donnee->create_at; ?></td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+
 @endsection
