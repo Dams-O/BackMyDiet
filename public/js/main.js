d4c87d021bee4c92f2015d1a874d5a8e9cf69e7d
Raphael <raphael.mathurin@ynov.com>
2019-11-08 17:36:49 +0100

    Carrousel + login button

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    $('body').vegas({
+        slides: [
+            {src: '../img/img1.jpg'},
+            {src: '../img/img2.jpg'},
+            {src: '../img/img3.jpg'}
+        ],
+        animation: 'random',
+        delay: 15000
+    });
+
+})
+
+
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,13 +3,16 @@
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>PHP Laravel web application</title>
+      <title>MyDiet Pro</title>
       <link href="{{ asset('css/gradients.css') }}" rel="stylesheet" type="text/css">
       <link href="{{ asset('css/styles.css') }}" rel="stylesheet" type="text/css">
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+      <link rel="stylesheet" href="js/vendor/vegas/vegas/vegas.min.css">
+      <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+      <script src="{{ asset('js/main.js')}}"></script>
     </head>
 
     <body>
@@ -25,44 +28,9 @@
                         <li><a href="login">Connection</a></li>
                         <li><a href="formulaire" class="home-link">Inscription</a></li>
                     </ul>
-
-            
                 </nav>
-            </header>
-
-        
-
-            <div class="container mt-3">
-                <div id="myCarousel" class="carousel slide">
-                
-                  <!-- Indicators -->
-                  <ul class="carousel-indicators">
-                    <li class="item1 active"></li>
-                    <li class="item2"></li>
-                    <li class="item3"></li>
-                  </ul>
-                  
-                  <!-- The slideshow -->
-                  <div class="carousel-inner">
-                    <div class="carousel-item active">
-                      <img src="{{ asset('img/img1.jpg') }}" alt="Los Angeles" width="1100" height="500">
-                    </div>
-                    <div class="carousel-item">
-                      <img src="{{ asset('img/img2.jpg') }}" alt="Chicago" width="1100" height="500">
-                    </div>
-                    <div class="carousel-item">
-                      <img src="{{ asset('img/img3.jpg') }}" alt="New York" width="1100" height="500">
-                    </div>
-                  </div>
-                  
-                </div>
-            </div>
-            <script src="{{ asset('js/carrousel.js')}}"></script>
-              
-              
-              
-              
-        
+            </header> 
+        <script src="js/vendor/vegas/vegas/vegas.min.js"></script>
     </body>
 
     
