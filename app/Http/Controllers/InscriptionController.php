ae7f3142368a11aa31ce2a6f95669e6f4df7918e
Antoine <antoine.romani@ynov.com>
2020-01-10 17:13:07 +0100

    Inscription complete

--- app/Http/Controllers/InscriptionController.php
+++ app/Http/Controllers/InscriptionController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+use App\User;
+use App\Http\Controllers\Store;
+use Auth;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 
 class InscriptionController extends Controller
 {
-    
+    public function form(){
+        return view('front.formulaire');
+
+    } 
+
+    public function register(Request $request){
+        $input = $request->all();
+        $user = new User; 
+        $user->last_name = $input['last_name'];
+        $user->first_name = $input['first_name'];
+        $user->pseudo = $input['user'];
+        $user->mail = $input['mail'];
+        $user->password = $input['pass'];
+        var_dump($user);
+
+        
+        $user->save();
+        
+    }
 }
