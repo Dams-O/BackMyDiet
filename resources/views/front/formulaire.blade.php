051f15f32ec47818b091f5304643ff2d9b67967e
antoine-rt <antoine.romani@ynov.com>
2020-02-14 10:31:41 +0100

    Form + Ajout recette

--- resources/views/front/formulaire.blade.php
+++ resources/views/front/formulaire.blade.php
@@ -1,28 +1,41 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>MyDiet - Connection</title>
-    </head>
-    <body>
+@extends('layout')
+
+@section('content')
     <div class="row">
-        <div class="col-md-12 center"><h1>Inscription</h1></div>
-        <div class="col-md-2">
-            <form class="form-inline" method="POST" action="/register">
-            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-            <div class="form-group">
-                    <label for="nom">Nom : </label>
-                    <input id="nom" type="text" name="last_name" placeholder="Nom" required> <br /> <br />
-                    <label for="prenom">Prenom : </label>
-                    <input id="prenom" type="text" name="first_name" placeholder="Prenom" required> <br /> <br />
-                    <label for="pseudo">Pseudo : </label>
-                    <input id="pseudo" type="text" name="user" placeholder="Pseudo" required> <br /> <br />
-                    <label for="email">Adresse mail : </label>
-                    <input id="email" type="text" name="mail" placeholder="exemple@gmail.com" required> <br /> <br />
-                    <label for="pass">Mot de passe : </label><input type="password" required name="pass" /> <br /> <br />
-                    <input type="submit" value="S'inscrire" />
-                </div>
-            </form>
+        <div class="col-xs-4 col-sm-4 col-lg-4"></div>
+        <div class="col-xs-5 col-sm-5 col-lg-5 text-center">
+            <div class="row">
+            <fieldset>
+                    <legend>Inscription</legend>
+                    <form class="form-inline" method="POST" action="/register">
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <div class="form-group">
+                        <div class="col-xs-12">
+                            <label for="nom">Nom : </label>
+                        </div>
+                            <input class="form form-control" id="nom" type="text" name="last_name" placeholder="Nom" required> <br/> <br/>
+                        <div class="col-xs-12">
+                            <label for="prenom">Prénom : </label>
+                        </div>
+                            <input class="form form-control" id="prenom" type="text" name="first_name" placeholder="Prenom" required> <br/> <br/>
+                        <div class="col-xs-12">
+                            <label for="pseudo">Pseudo : </label>
+                        </div>
+                            <input class="form form-control" id="pseudo" type="text" name="user" placeholder="Pseudo" required> <br/> <br/>
+                        <div class="col-xs-12">
+                            <label for="email">Adresse mail : </label>
+                        </div>
+                            <input class="form form-control" id="email" type="text" name="mail" placeholder="exemple@gmail.com" required><br/> <br/>
+                        <div class="col-xs-12"> 
+                            <label for="pass">Mot de passe : </label>
+                        </div>
+                            <input class="form form-control" id="password" type="password" required name="pass" placeholder="Mot de passe" /> <br/> <br/>
+                            <input class="btn btn-primary" type="submit" value="S'inscrire" />
+                        </div>
+                    </form>
+            </fieldset>
+            </div>
         </div>
+        <div class="col-xs-4 col-sm-4 col-lg-4"></div>
     </div>
-    </body>
+@endsection    
