770631aae247a9d4797dc45a858b149ea2e82bf4
antwane <antoine.despres@ynov.com>
2020-11-25 16:55:21 +0100

    Fix layout Fix css

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,77 +1,11 @@
-@extends('layout')
+@extends('layout.layout')
+
+@section('custom_css')
+<link rel="stylesheet" href="{{URL::asset('css/search.css')}}">
+@endsection
 
 
 @section('content')
-<style>
-    @font-face{
-      font-family: "Montserrat-Reg";
-      src: url(../fonts/Montserrat-Regular.ttf);
-    }
-
-    @font-face{
-      font-family: "Montserrat-Bold";
-      src: url(../fonts/Montserrat-Bold.ttf);
-    }
-
-    body {
-        background: url(img/search/fond.jpg) no-repeat center center;
-        background-size: 100%;
-        background-position-y: 10%; 
-        background-attachment: fixed;
-    }
-    img#logo { width: 100px; }
-    ul.search_resultList {list-style: none; margin: 0; padding: 0; }
-    input#search_Field {
-        padding: 0;
-        outline: none;
-        border: none;
-        background: url(img/background-input.png) no-repeat center center;
-        background-size: 100% 100%;
-        text-align: center;
-        width: 100%;
-        height: 90px;
-    }
-
-    input[type=checkbox]{
-        display: none;
-    }
-
-    form.search_Result > div {
-        width: 500px;
-    }
-    label img { width: 30px; } 
-
-
-    div.search_formContain {
-        z-index: 99;
-        height: 100%;
-        width: 830px;
-        background: url(img/search/searchField.png) no-repeat top center;
-        background-size: 95% 95%;
-        background-position: 50% 17px;
-        opacity: 0.8;
-
-    }
-
-    ul.search_resultList > li {
-        z-index: 100;
-        width: 750px;
-        display: flex;
-        justify-content: space-between;
-    }
-
-    .searchTitle {
-        font-family: Montserrat-Bold;
-        color: white;
-        font-size: 30px;
-    }
-
-    form {
-        font-family: Montserrat-Reg;
-        font-weight: bold;
-        color: grey;
-    }
-</style>
 
 <div id="headerWrap">
     <nav class="navbar navbar-light navbar-expand-md navigation-clean">
@@ -87,8 +21,8 @@
 </div>
 
 <div class="content d-flex flex-column align-items-center text-center">
-    <span class="searchTitle">Vous cherchez quelqu'un ?</span><br />
-    <div class="search_formContain"/>
+    <span class="search_Title">Vous cherchez quelqu'un ?</span><br />
+    <div class="search_formContain">
         <input id="search_Field" type="text" />
         <form class="search_Result text-center" method="GET" action="#">
             <div class="d-inline-flex justify-content-center form-group">
